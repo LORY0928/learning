59860c38f42f965e56a4c7b692f86e2e2e63c45f
LORY0928 <1537949646@qq.com>
2020-12-08 21:44:35 +0800

    根据评价做出正确修改

--- index.js
+++ index.js
@@ -10,19 +10,21 @@ router.prefix("/api");
 router.post("/user", (ctx) => {
   const { role } = ctx.request.header;
   const { name, email } = ctx.request.body;
-  const resultBody = [
-    { code: 404, msg: "404" },
-    { code: 404, msg: "name与email不能为空" },
-    { code: 401, msg: "unauthorized post" },
-    { code: 200, msg: "上传成功", data: { name: name, email: email } },
-  ];
-  let i = 0;
-  if (role === "admin" && name === "imooc" && email === "imooc@email.com")
-    i = 3;
-  if (!role || role !== "admin") i = 2;
-  if (!name || !email) i = 1;
-  console.log("index: ", i);
-  ctx.body = resultBody[i];
+  if (!name || !email) {
+    ctx.body = { code: 404, msg: "name与email不能为空" };
+  } else if (
+    role === "admin" &&
+    name === "imooc" &&
+    email === "imooc@email.com"
+  ) {
+    ctx.body = {
+      code: 200,
+      msg: "上传成功",
+      data: { name: name, email: email },
+    };
+  } else if (!role || role !== "admin") {
+    ctx.body = { code: 401, msg: "unauthorized post" };
+  }
 });
 
 app.use(koaBody());
